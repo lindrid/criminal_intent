a77aeb020b5d715e10d7aaf5ad55519165bfb366
lindrid <lindrid32@gmail.com>
2021-04-30 06:02:59 -0400

    264

---
+++ app/src/main/java/com/example/criminal_intent/database/CrimeDatabase.kt
@@ -0,0 +1,11 @@
+package com.example.criminal_intent.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.criminal_intent.Crime
+
+@Database(entities = [Crime::class], version = 1)
+@TypeConverters(CrimeTypeConverters::class)
+abstract class CrimeDatabase: RoomDatabase() {
+}
